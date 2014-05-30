2d8dfcdc279d1de356afc890f38cd7224696b061
Joseph McGlynn <jmcglynn4@comcast.net>
2014-05-30 17:48:39 -0600

    fix compile warnings on unused code and missing return type discovered on mac

--- saParser.c
+++ saParser.c
@@ -416,25 +416,6 @@ void tokenize(char *line, saTokenTypePtr tokenStack[], int *tokenCount, char *er
 
     return;
 }
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
-{
-    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
-    result->tv_sec = diff / 1000000;
-    result->tv_usec = diff % 1000000;
-
-    return (diff<0);
-}
-void timeval_print(FILE *f, struct timeval *tv)
-{
-    char buffer[30];
-    time_t curtime;
-
-    fprintf(f,"%ld.%06ld", tv->tv_sec, tv->tv_usec);
-    curtime = tv->tv_sec;
-    strftime(buffer, 30, "%m-%d-%Y  %T", localtime(&curtime));
-    fprintf(f, " = %s.%06ld\n", buffer, tv->tv_usec);
-}
-
 
 saExpressionTypePtr andExpression(saExpressionTypePtr left, saTokenTypePtr currToken) 
 {
