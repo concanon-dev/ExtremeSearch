64274499cd8d061d1e0638ea4bf7ada0eac34f65
Michael Cormier <github@anterus.com>
2014-06-10 20:35:50 -0700

    fixed xsListContexts

--- python/xsListContexts.py
+++ python/xsListContexts.py
@@ -9,16 +9,18 @@ import splunk.Intersplunk as si
 
 if __name__ == '__main__':
 
-    scope = 3
+    scope = '3'
     try:
         if len(sys.argv) < 1:
             raise Exception("xsListContexts-F-001: Usage: xsListContexts [scope]") 
 
-        scope = sys.argv[1]
-        scope = scope.lower()
+        if len(sys.argv) == 2:
+            scope = sys.argv[1]
+            scope = scope.lower()
+
         binary = os.environ["SPLUNK_HOME"] + "/etc/apps/xtreme/bin/" +  platform.system() + "/" + platform.architecture()[0] + "/xsListContexts"
-#        if not os.path.isfile(binary):
-#            raise Exception("xsListContexts-F-000: Can't find binary file " + binary)
+        if not os.path.isfile(binary):
+            raise Exception("xsListContexts-F-000: Can't find binary file " + binary)
 
         subprocess.call([binary, '-s', scope])
 
