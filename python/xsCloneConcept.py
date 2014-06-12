38b2167cc1b99123e8ad42c3062b061d4eaad887
Joseph McGlynn <jmcglynn4@comcast.net>
2014-06-12 17:15:23 -0600

    add exe for windows to clone concept

--- python/xsCloneConcept.py
+++ python/xsCloneConcept.py
@@ -49,6 +49,8 @@ if __name__ == '__main__':
             raise Exception("xsCloneConcept-F-001: Usage: xsCloneConcept oldConcept IN oldContext TO newConcept IN newContext");
 
         binary = os.environ["SPLUNK_HOME"] + "/etc/apps/xtreme/bin/" +  platform.system() + "/" + platform.architecture()[0] + "/xsCloneConcept"
+        if (platform.system() == 'Windows'):
+            binary = binary + ".exe"
         if not os.path.isfile(binary):
             raise Exception("xsCloneConcept-F-000: Can't find binary file " + binary)
 
