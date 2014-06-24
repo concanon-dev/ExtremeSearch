f63ae459d09ec0b83bedf3fc32959160657283ff
Joseph McGlynn <jmcglynn4@comcast.net>
2014-06-24 16:59:59 -0600

    started saContext_unittest and edit to perform.expect

---
+++ gtest/tests/saContext_unittest.cc
@@ -0,0 +1,45 @@
+// Copyright 2014, Scianta Analytics LLC
+// All rights reserved.
+//
+// Author: joseph.mcglynn@sciantaanalytics.com (Joseph McGlynn)
+
+#include <iostream>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include "../../saContext.h"
+#include "gtest/gtest.h"
+
+extern "C" {
+saContextTypePtr saContextInit(char *name, double domainMin, double domainMax, double avg, double sdev, int count, int numConcepts, char *type, char *notes, char *uom);
+saContextTypePtr saContextLoad(FILE *infile);
+saContextTypePtr saContextMerge(saContextTypePtr c1, saContextTypePtr c2, char *c3Name);
+char *eatTimestamp(char **cursor);
+char *getLine(FILE *infile);
+char *getAttribute(char **cursor, char separator);
+char *getValue(char **cursor);
+bool saContextSave(FILE *outfile, saContextTypePtr contextPtr);
+time_t getTimestamp(char *buf);
+char *trim(char *str);
+void saContextDisplayWithHeader(saContextTypePtr contextPtr, char *name);
+void saContextDisplay(saContextTypePtr contextPtr);
+void saContextLookup(saContextTypePtr contextPtr, double value, double *results);
+}
+
+TEST(SAContext, saContextLoad) {
+
+    char path[256] = "etc/apps/xtreme/contexts/TEST.context";
+    FILE *f = fopen(path, "r");
+    saContextTypePtr contextPtr = saContextLoad(f);
+    fclose(f);
+
+    EXPECT_STREQ("TEST", contextPtr->name);
+    EXPECT_STREQ("v2.0", contextPtr->format);
+
+}
+
--- gtest/tests/saParser_unittest.cc
+++ gtest/tests/saParser_unittest.cc
@@ -67,6 +67,74 @@ TEST(ParserTest, IN_Clause) {
     EXPECT_STREQ(expStack[4]->field, "is");
 }
 
+TEST(ParserTest, AND_Clause) {
+
+    char whereLine[MAXROWSIZE / 32] = "(Height in MaleHeight is not tiny) and (Height in MaleHeight is not short)";
+    char tempbuf[MAXROWSIZE];
+    saExpressionTypePtrArray expStack = generateExpStack();
+    int stackSize = saParserParse(whereLine, tempbuf, expStack);
+
+    EXPECT_EQ(stackSize, 13);
+    EXPECT_EQ(expStack[0]->type, SA_TOKEN_FIELD);
+    EXPECT_STREQ(expStack[0]->field, "Height");
+    EXPECT_EQ(expStack[1]->type, SA_TOKEN_FUZZYTERMSET);
+    EXPECT_STREQ(expStack[1]->field, "MaleHeight");
+    EXPECT_EQ(expStack[2]->type, SA_TOKEN_IN);
+    EXPECT_STREQ(expStack[2]->field, "in");
+    EXPECT_EQ(expStack[3]->type, SA_TOKEN_FUZZYSET);
+    EXPECT_STREQ(expStack[3]->field, "tiny");
+    EXPECT_EQ(expStack[4]->type, SA_TOKEN_MODIFIER);
+    EXPECT_STREQ(expStack[4]->field, "not");
+    EXPECT_EQ(expStack[5]->type, SA_TOKEN_IS);
+    EXPECT_STREQ(expStack[5]->field, "is");
+    EXPECT_EQ(expStack[6]->type, SA_TOKEN_FIELD);
+    EXPECT_STREQ(expStack[6]->field, "Height");
+    EXPECT_EQ(expStack[7]->type, SA_TOKEN_FUZZYTERMSET);
+    EXPECT_STREQ(expStack[7]->field, "MaleHeight");
+    EXPECT_EQ(expStack[8]->type, SA_TOKEN_IN);
+    EXPECT_STREQ(expStack[8]->field, "in");
+    EXPECT_EQ(expStack[9]->type, SA_TOKEN_FUZZYSET);
+    EXPECT_STREQ(expStack[9]->field, "short");
+    EXPECT_EQ(expStack[10]->type, SA_TOKEN_MODIFIER);
+    EXPECT_STREQ(expStack[10]->field, "not");
+    EXPECT_EQ(expStack[11]->type, SA_TOKEN_IS);
+    EXPECT_STREQ(expStack[11]->field, "is");
+    EXPECT_EQ(expStack[12]->type, SA_TOKEN_AND);
+    EXPECT_STREQ(expStack[12]->field, "and");
+}
+
+TEST(ParserTest, OR_Clause) {
+
+    char whereLine[MAXROWSIZE / 32] = "(Height in MaleHeight is tiny) or (Height in MaleHeight is short)";
+    char tempbuf[MAXROWSIZE];
+    saExpressionTypePtrArray expStack = generateExpStack();
+    int stackSize = saParserParse(whereLine, tempbuf, expStack);
+
+    EXPECT_EQ(stackSize, 11);
+    EXPECT_EQ(expStack[0]->type, SA_TOKEN_FIELD);
+    EXPECT_STREQ(expStack[0]->field, "Height");
+    EXPECT_EQ(expStack[1]->type, SA_TOKEN_FUZZYTERMSET);
+    EXPECT_STREQ(expStack[1]->field, "MaleHeight");
+    EXPECT_EQ(expStack[2]->type, SA_TOKEN_IN);
+    EXPECT_STREQ(expStack[2]->field, "in");
+    EXPECT_EQ(expStack[3]->type, SA_TOKEN_FUZZYSET);
+    EXPECT_STREQ(expStack[3]->field, "tiny");
+    EXPECT_EQ(expStack[4]->type, SA_TOKEN_IS);
+    EXPECT_STREQ(expStack[4]->field, "is");
+    EXPECT_EQ(expStack[5]->type, SA_TOKEN_FIELD);
+    EXPECT_STREQ(expStack[5]->field, "Height");
+    EXPECT_EQ(expStack[6]->type, SA_TOKEN_FUZZYTERMSET);
+    EXPECT_STREQ(expStack[6]->field, "MaleHeight");
+    EXPECT_EQ(expStack[7]->type, SA_TOKEN_IN);
+    EXPECT_STREQ(expStack[7]->field, "in");
+    EXPECT_EQ(expStack[8]->type, SA_TOKEN_FUZZYSET);
+    EXPECT_STREQ(expStack[8]->field, "short");
+    EXPECT_EQ(expStack[9]->type, SA_TOKEN_IS);
+    EXPECT_STREQ(expStack[9]->field, "is");
+    EXPECT_EQ(expStack[10]->type, SA_TOKEN_OR);
+    EXPECT_STREQ(expStack[10]->field, "or");
+}
+
 /*
 TEST(ParserTest, BY_Clause) {
     char whereLine[MAXROWSIZE / 32] = "Height by Gender is Tall";
