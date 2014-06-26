f4b1f722250c4ea9d269070ac16b2518adcdd505
Michael Cormier <github@anterus.com>
2014-06-26 18:43:01 -0400

    fixed -m in xspreuatoregress

--- python/xspreautoregress.py
+++ python/xspreautoregress.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
 
     try:
         b=''
-        u=''
         x=''
 
         if len(sys.argv) >1:
@@ -19,14 +18,11 @@ if __name__ == '__main__':
                 if arg.lower().startswith("b="):
                     eqsign = arg.find('=')
                     b = arg[eqsign+1:len(arg)]
-                if arg.lower().startswith("u="):
-                    eqsign = arg.find('=')
-                    u = arg[eqsign+1:len(arg)]
                 if arg.lower().startswith("x="):
                     eqsign = arg.find('=')
                     x = arg[eqsign+1:len(arg)]
         else:
-            raise Exception("xspreautoregress-F-001: Usage: xspreautoregress x=\"field1,field2...\" [u=method] [b=field]")
+            raise Exception("xspreautoregress-F-001: Usage: xspreautoregress x=\"field1,field2...\" [b=field]")
 
         if len(x) < 1:
             raise Exception("xspreautoregress-F-003: Missing x parameter")
@@ -38,9 +34,9 @@ if __name__ == '__main__':
             raise Exception("xspreautoregress-F-000: Can't find binary file " + binary)
 
         if b != '':
-            subprocess.call([binary, '-b', b, '-i', '-m', u, '-x', x ])
+            subprocess.call([binary, '-b', b, '-i', '-x', x ])
         else:
-            subprocess.call([binary, '-i', '-m', u, '-x', x ])
+            subprocess.call([binary, '-i', '-x', x ])
 
         if platform.system() == 'Windows':
             sys.stdout.flush()
