53f447cebe4acb231541190f1ecc744708d45f8e
Michael Cormier <github@anterus.com>
2014-07-05 12:57:25 -0400

    fixed typos

--- csv3.h
+++ csv3.h
@@ -1,3 +1,9 @@
+/*
+ Copyright 2012-2014 Scianta Analytics LLC   All Rights Reserved.  
+ Reproduction or unauthorized use is prohibited. Unauthorized
+ use is illegal. Violators will be prosecuted. This software 
+ contains proprietary trade and business secrets.      
+*/
 #ifndef SA_CSV_H_3
 #define SA_CSV_H_3
 #include <stdio.h>
--- xsPerformAutoRegression.c
+++ xsPerformAutoRegression.c
@@ -1,5 +1,5 @@
 /*
- Copyrigh 2012-2014 Scianta Analytics LLC   All Rights Reserved.  
+ Copyright 2012-2014 Scianta Analytics LLC   All Rights Reserved.  
  Reproduction or unauthorized use is prohibited. Unauthorized
  use is illegal. Violators will be prosecuted. This software 
  contains proprietary trade and business secrets.            
